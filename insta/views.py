62bef6fe67b141acecd28888f9605f917a362c8e
Mantongash <you@example.com>
2020-02-09 15:36:20 +0300

    Applied code to submit the sign up form in the views.py

--- insta/views.py
+++ insta/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
+  if request.method == "POST":
+    form=UserCreationForm(request.POST)
+    if form.is_valid:
+      username = form.cleaned_data.get("username")
+      messages.success(request, f("Account for {username} has been created successfully"))
+      return redirect("insta-home")
+  else:
   form=UserCreationForm()
   return render(request, "insta/index.html",{"form":form})
