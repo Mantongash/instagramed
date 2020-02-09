86c3c2dc46074ff737078720ab6a83734ef38662
Mantongash <you@example.com>
2020-02-09 12:54:36 +0300

    Imported UserCreationForm from django and applied it in the index view

--- insta/views.py
+++ insta/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def index(request):
-  return render(request, "insta/index.html",{})
+  form=UserCreationForm()
+  return render(request, "insta/index.html",{"form":form})
