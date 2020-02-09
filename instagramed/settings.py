c67da7bda1dd9135b4c1aa4d0cdea613967e92c4
Mantongash <you@example.com>
2020-02-09 20:38:28 +0300

    Registered the account app in the settings

--- instagramed/settings.py
+++ instagramed/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'account.apps.AccountConfig',
     'insta.apps.InstaConfig',
     'crispy_forms',
     'django.contrib.admin',
